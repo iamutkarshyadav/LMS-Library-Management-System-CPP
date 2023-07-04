52570a7592cd07b96fb630702826e701ecfde33b
iamutkarshyadav <72760223+iamutkarshyadav@users.noreply.github.com>
2023-07-04 23:42:30 +0530

    book

--- LMS.cpp
+++ LMS.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
-#include<iomanip>
+#include <iomanip>
+#include <vector>
 
 using namespace std;
 
@@ -14,36 +15,79 @@ public:
     int edition;
     int publishYear;
     int numberOfBooks;
+    vector<BookEntry> books; // Store multiple books
 
-    void registerBook(int i);
+    void printData();
+    void registerBook();
 };
 
-
-class Options : public BookEntry
-{
-public:
-    BookEntry b;
-    void options();
-};
-
-void BookEntry::registerBook(int i)
+void BookEntry::registerBook()
 {
     cout << "Enter the number of Books this time:\n";
     cin >> numberOfBooks;
-    for (int i = 0; i <= numberOfBooks; ++i)
+
+    for (int i = 0; i < numberOfBooks; ++i)
     {
         cout << "===============================\n";
         cout << "     CLI for Book Entry\n";
         cout << "===============================\n";
-        cout << "Enter the Name of the Book \n";
+        cout << "Enter the Name of the Book: ";
         cin >> bookName;
-        cout << "Enter the ISBN number of the Book\n";
+        cout << "Enter the Author of the Book: ";
+        cin >> bookAuthor;
+        cout << "Enter the Publisher: ";
+        cin >> publisher;
+        cout << "Enter the ISBN number of the Book: ";
         cin >> ISBN;
-        cout << "Enter the Publication\n";
-        cout << "Enter the Edition (numbers only)\n";
+        cout << "Enter the Edition (numbers only): ";
         cin >> edition;
+
+        // Create a book object and add it to the vector of books
+        BookEntry book;
+        book.bookName = bookName;
+        book.bookAuthor = bookAuthor;
+        book.publisher = publisher;
+        book.ISBN = ISBN;
+        book.edition = edition;
+        books.push_back(book);
     }
 }
+
+void BookEntry::printData()
+{
+    cout << "=========================================\n";
+    cout << "           Books in Library\n";
+    cout << "=========================================\n";
+
+    if (books.empty())
+    {
+        cout << "No books available.\n";
+        return;
+    }
+
+    cout << left << setw(25) << "Title"
+         << setw(25) << "Author"
+         << setw(25) << "Publication"
+         << setw(15) << "ISBN"
+         << setw(10) << "Edition"
+         << "\n";
+
+    for (const auto &book : books)
+    {
+        cout << left << setw(25) << book.bookName
+             << setw(25) << book.bookAuthor
+             << setw(25) << book.publisher
+             << setw(15) << book.ISBN
+             << setw(10) << book.edition
+             << "\n";
+    }
+}
+
+class Options : public BookEntry
+{
+public:
+    void options();
+};
 
 void Options::options()
 {
@@ -58,7 +102,8 @@ void Options::options()
     switch (opts)
     {
     case 1:
-        b.registerBook(1);
+        registerBook();
+        printData();
         break;
     }
 }
